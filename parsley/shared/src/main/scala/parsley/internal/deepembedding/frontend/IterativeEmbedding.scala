4be61d766753f234bd63f1541a7f633e734dddd9
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-06-05 12:33:39 +0100

    refactor: major changes to fix warnings in compilation
    - Added @nowarn to code with unavoidable runtime generic type matching
    - Added the attachReusable family of combinators to expose an API that
      allows the user to more easily use debugged parsers as children to
      mulitple other parsers
    - Removed Iterative trait as it is now obsolete
    - Added explanatory comments for certain "hacks"
    - General duplicated code extraction

--- parsley/shared/src/main/scala/parsley/internal/deepembedding/frontend/IterativeEmbedding.scala
+++ parsley/shared/src/main/scala/parsley/internal/deepembedding/frontend/IterativeEmbedding.scala
@@ -6,20 +6,16 @@ package parsley.internal.deepembedding.frontend
 import parsley.internal.deepembedding.ContOps, ContOps.{suspend, ContAdapter}
 import parsley.internal.deepembedding.backend, backend.StrictParsley
 
-// Marks a parser as being explicitly iterative.
-// It does nothing at all other than that.
-sealed trait Iterative
-
-private [parsley] final class Many[A](p: LazyParsley[A]) extends Unary[A, List[A]](p) with Iterative {
+private [parsley] final class Many[A](p: LazyParsley[A]) extends Unary[A, List[A]](p) {
     override def make(p: StrictParsley[A]): StrictParsley[List[A]] = new backend.Many(p)
 }
-private [parsley] final class SkipMany[A](p: LazyParsley[A]) extends Unary[A, Unit](p) with Iterative {
+private [parsley] final class SkipMany[A](p: LazyParsley[A]) extends Unary[A, Unit](p) {
     override def make(p: StrictParsley[A]): StrictParsley[Unit] = new backend.SkipMany(p)
 }
-private [parsley] final class ChainPost[A](p: LazyParsley[A], _op: =>LazyParsley[A => A]) extends Binary[A, A => A, A](p, _op) with Iterative {
+private [parsley] final class ChainPost[A](p: LazyParsley[A], _op: =>LazyParsley[A => A]) extends Binary[A, A => A, A](p, _op) {
     override def make(p: StrictParsley[A], op: StrictParsley[A => A]): StrictParsley[A] = new backend.ChainPost(p, op)
 }
-private [parsley] final class ChainPre[A](p: LazyParsley[A], op: LazyParsley[A => A]) extends LazyParsley[A] with Iterative {
+private [parsley] final class ChainPre[A](p: LazyParsley[A], op: LazyParsley[A => A]) extends LazyParsley[A] {
     def itemParser: LazyParsley[A] = p
 
     def opParser: LazyParsley[A => A] = op
@@ -34,19 +30,19 @@ private [parsley] final class ChainPre[A](p: LazyParsley[A], op: LazyParsley[A =
         } yield new backend.ChainPre(p, op)
 }
 private [parsley] final class Chainl[A, B](init: LazyParsley[B], p: =>LazyParsley[A], op: =>LazyParsley[(B, A) => B])
-    extends Ternary[B, A, (B, A) => B, B](init, p, op) with Iterative {
+    extends Ternary[B, A, (B, A) => B, B](init, p, op) {
     override def make(init: StrictParsley[B], p: StrictParsley[A], op: StrictParsley[(B, A) => B]): StrictParsley[B] = new backend.Chainl(init, p, op)
 }
 private [parsley] final class Chainr[A, B](p: LazyParsley[A], op: =>LazyParsley[(A, B) => B], private [Chainr] val wrap: A => B)
-    extends Binary[A, (A, B) => B, B](p, op) with Iterative {
+    extends Binary[A, (A, B) => B, B](p, op) {
     override def make(p: StrictParsley[A], op: StrictParsley[(A, B) => B]): StrictParsley[B] = new backend.Chainr(p, op, wrap)
 }
-private [parsley] final class SepEndBy1[A, B](p: LazyParsley[A], sep: =>LazyParsley[B]) extends Binary[A, B, List[A]](p, sep) with Iterative {
+private [parsley] final class SepEndBy1[A, B](p: LazyParsley[A], sep: =>LazyParsley[B]) extends Binary[A, B, List[A]](p, sep) {
     override def make(p: StrictParsley[A], sep: StrictParsley[B]): StrictParsley[List[A]] = new backend.SepEndBy1(p, sep)
 }
-private [parsley] final class ManyUntil[A](body: LazyParsley[Any]) extends Unary[Any, List[A]](body) with Iterative {
+private [parsley] final class ManyUntil[A](body: LazyParsley[Any]) extends Unary[Any, List[A]](body) {
     override def make(p: StrictParsley[Any]): StrictParsley[List[A]] = new backend.ManyUntil(p)
 }
-private [parsley] final class SkipManyUntil(body: LazyParsley[Any]) extends Unary[Any, Unit](body) with Iterative {
+private [parsley] final class SkipManyUntil(body: LazyParsley[Any]) extends Unary[Any, Unit](body) {
     override def make(p: StrictParsley[Any]): StrictParsley[Unit] = new backend.SkipManyUntil(p)
 }
